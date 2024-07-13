1f911d3df11c8e821cd2d2beae52dbdf011e2385
Derek King <king.dm49@gmail.com>
2024-07-14 01:11:38 +0800

    Support up to 4 fans. Renumbered pin names to be 0-indexed to match data arrays

--- Fan_speed.ino
+++ Fan_speed.ino
@@ -2,7 +2,7 @@
 //   Digispark Pro    (One fan)
 //   Arduino Uno/Nano (One fan)
 //   Teensy 2.0       (Two fans)
-//   Arduino Mega     (Four fans, though only two are configured in this code)
+//   Arduino Mega     (Four fans)
 //   A board with one 16-bit timer and free hardware inturrupt pin per fan configured
 
 // Uses Timer1/3/4/5 for each fan respectively as these are usually the 16-bit timers if the board has them
@@ -14,40 +14,41 @@
   // Extra info:
   //  https://github.com/SpenceKonde/ATTinyCore/blob/v2.0.0-devThis-is-the-head-submit-PRs-against-this/avr/extras/ATtiny_x7.md
   //  https://github.com/SpenceKonde/ATTinyCore/blob/c0b7e78f750111bbfeeb36088ef9be3d5728a927/avr/variants/tinyx7_digi/pins_arduino.h
-  #define IN_PIN_1  PA3  // INT1/Digital pin 9 on the Digispark Pro
-  #define OUT_PIN_1 5    // Any other non-important pin
+  #define IN_PIN_0  PA3  // INT1/Digital pin 9 on the Digispark Pro
+  #define OUT_PIN_0 5    // Any other non-important pin
 
 #elif defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_NANO)
-  #define IN_PIN_1  3  // INT1
-  #define OUT_PIN_1 5  // Any other non-important pin
+  #define IN_PIN_0  3  // INT1
+  #define OUT_PIN_0 5  // Any other non-important pin
 
 #elif defined(__AVR_ATmega32U4__)  // Teensy 2.0 etc.
-  // #define IN_PIN_1  2  // INT1
-  // #define OUT_PIN_1 ?  // Any other non-important pin
+  #define IN_PIN_0  5  // INT0
+  #define OUT_PIN_0 4  // Any other non-important pin
 
-  // #define IN_PIN_2  0  // INT2
-  // #define OUT_PIN_2 ?  // Any other non-important pin
+  #define IN_PIN_1  6  // INT1
+  #define OUT_PIN_1 7  // Any other non-important pin
 
 #elif defined(ARDUINO_AVR_MEGA2560)
   // INTx as defined by the Arduino library rather than the chip:
   //  https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/#_interrupt_numbers
-  #define IN_PIN_1  3  // INT1
-  #define OUT_PIN_1 5  // Any other non-important pin
+  #define IN_PIN_0  3  // INT1
+  #define OUT_PIN_0 5  // Any other non-important pin
 
-  #define IN_PIN_2 21  // INT2
-  #define OUT_PIN_2 7  // Any other non-important pin
+  #define IN_PIN_1 21  // INT2
+  #define OUT_PIN_1 7  // Any other non-important pin
 
-  #define IN_PIN_3 20  // INT3
-  #define OUT_PIN_3 9  // Any other non-important pin
+  #define IN_PIN_2 20  // INT3
+  #define OUT_PIN_2 9  // Any other non-important pin
 
-  #define IN_PIN_4 19  // INT4
-  #define OUT_PIN_4 11 // Any other non-important pin
+  #define IN_PIN_3 19  // INT4
+  #define OUT_PIN_3 11 // Any other non-important pin
 
 #else
   // Define custom boards here
   //  https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
-  // #define IN_PIN_1  ?  // INT1/2/3/... (A free hardware interrupt pin)
-  // #define OUT_PIN_1 ?  // Any other non-important pin
+  // #define IN_PIN_0  ?  // INT0/1/2/3/... (A free hardware interrupt pin)
+  // #define OUT_PIN_0 ?  // Any other non-important pin
+
 #endif
 
 // Time in milliseconds between updates to the RPM (longer than 6 sec can cause overflow)
@@ -125,13 +126,14 @@ uint16_t toggleB[4] = {2343, 2343, 2343, 2343};
 void setup() {
     cli();  // disable global interrupts
 
-#ifdef IN_PIN_1
-    pinMode(IN_PIN_1, INPUT_PULLUP);
-    // Connect OUT_PIN_1 to ground
-    pinMode(OUT_PIN_1, OUTPUT);
-    digitalWrite(OUT_PIN_1, LOW);
+#ifdef IN_PIN_0
+    // Timer1
+    pinMode(IN_PIN_0, INPUT_PULLUP);
+    // Connect OUT_PIN_0 to ground
+    pinMode(OUT_PIN_0, OUTPUT);
+    digitalWrite(OUT_PIN_0, LOW);
 
-    attachInterrupt(digitalPinToInterrupt(IN_PIN_1), []() {halfRevolution(0);}, RISING);
+    attachInterrupt(digitalPinToInterrupt(IN_PIN_0), []() {halfRevolution(0);}, RISING);
 
     TCCR1A = 0;  // Disconnect output pins from timer 1
     TCCR1B = (1 << WGM12);  // Set Timer 1 to CTC mode
@@ -143,16 +145,38 @@ void setup() {
     TCCR1B |= PRESCALER;
 #endif
 
-#ifdef IN_PIN_2
-    pinMode(IN_PIN_2, INPUT_PULLUP);
-    pinMode(OUT_PIN_2, OUTPUT); digitalWrite(OUT_PIN_2, LOW);
-    attachInterrupt(digitalPinToInterrupt(IN_PIN_2), []() {halfRevolution(1);}, RISING);
+#ifdef IN_PIN_1
+    // Timer3
+    pinMode(IN_PIN_1, INPUT_PULLUP);
+    pinMode(OUT_PIN_1, OUTPUT); digitalWrite(OUT_PIN_1, LOW);
+    attachInterrupt(digitalPinToInterrupt(IN_PIN_1), []() {halfRevolution(1);}, RISING);
     // Skip to Timer3 since Timer2 seems to usually be 8-bit by convention
     TCCR3A = 0;  // Disconnect output pins from timer 3
     TCCR3B = (1 << WGM32) | PRESCALER;  // Set Timer 3 to CTC mode and add prescaler
-    OCR3A = toggleA[1];  // Set 16-bit Output Compare Register A (TOP value / High trigger time)
-    OCR3B = toggleB[1];  // Set 16-bit Output Compare Register B (Low trigger time)
+    OCR3A = toggleA[1]; OCR3B = toggleB[1];
     TIMSK3 |= (1 << OCIE3A) | (1 << OCIE3B);
+#endif
+
+#ifdef IN_PIN_2
+    // Timer4
+    pinMode(IN_PIN_2, INPUT_PULLUP);
+    pinMode(OUT_PIN_2, OUTPUT); digitalWrite(OUT_PIN_2, LOW);
+    attachInterrupt(digitalPinToInterrupt(IN_PIN_2), []() {halfRevolution(2);}, RISING);
+    TCCR4A = 0;  // Disconnect output pins from timer 4
+    TCCR4B = (1 << WGM42) | PRESCALER;  // Set Timer 4 to CTC mode and add prescaler
+    OCR4A = toggleA[2]; OCR4B = toggleB[2];
+    TIMSK4 |= (1 << OCIE4A) | (1 << OCIE4B);
+#endif
+
+#ifdef IN_PIN_3
+    // Timer5
+    pinMode(IN_PIN_3, INPUT_PULLUP);
+    pinMode(OUT_PIN_3, OUTPUT); digitalWrite(OUT_PIN_3, LOW);
+    attachInterrupt(digitalPinToInterrupt(IN_PIN_3), []() {halfRevolution(3);}, RISING);
+    TCCR5A = 0;  // Disconnect output pins from timer 5
+    TCCR5B = (1 << WGM52) | PRESCALER;  // Set Timer 5 to CTC mode and add prescaler
+    OCR5A = toggleA[3]; OCR5B = toggleB[3];
+    TIMSK5 |= (1 << OCIE5A) | (1 << OCIE5B);
 #endif
 
     sei();  // re-enable global interrupts
@@ -161,12 +185,20 @@ void setup() {
 void loop() {
     delay(UPDATE_INTERVAL_MS);
 
-#ifdef IN_PIN_1
+#ifdef IN_PIN_0
     calculateOutputInterval(0);
 #endif
 
-#ifdef IN_PIN_2
+#ifdef IN_PIN_1
     calculateOutputInterval(1);
+#endif
+
+#ifdef IN_PIN_2
+    calculateOutputInterval(2);
+#endif
+
+#ifdef IN_PIN_3
+    calculateOutputInterval(3);
 #endif
 }
 
@@ -187,16 +219,15 @@ void halfRevolution(byte fanIndex) {
     unsigned long currentMicros = micros();
     // Add some smoothing to the speed
     avgInterval[fanIndex] = (ALPHA*(currentMicros-previousMicros[fanIndex])) + (ONE_MINUS_ALPHA*avgInterval[fanIndex]);
-    // avgInterval[fanIndex] = currentMicros - previousMicros[fanIndex];
 
     previousMicros[fanIndex] = currentMicros;
 }
 
 
-#ifdef IN_PIN_1
+#ifdef IN_PIN_0
 ISR(TIMER1_COMPA_vect) {
-    // Interrupt releasing OUT_PIN_1 (Floating/Letting PC pull HIGH)
-    pinMode(OUT_PIN_1, INPUT);
+    // Interrupt releasing OUT_PIN_0 (Floating/Letting PC pull HIGH)
+    pinMode(OUT_PIN_0, INPUT);
     // OCR1A/B are set here to simulate double buffering that avoids
     // setting TOP lower than the current timer and thus missing the interrupt
     if (OCR1A != toggleA[0]) {
@@ -205,20 +236,46 @@ ISR(TIMER1_COMPA_vect) {
     }
 }
 ISR(TIMER1_COMPB_vect) {
-    // Interrupt connecting OUT_PIN_1 to ground (LOW)
-    pinMode(OUT_PIN_1, OUTPUT);
+    // Interrupt connecting OUT_PIN_0 to ground (LOW)
+    pinMode(OUT_PIN_0, OUTPUT);
 }
 #endif
 
-#ifdef IN_PIN_2
+#ifdef IN_PIN_1
 ISR(TIMER3_COMPA_vect) {
-    pinMode(OUT_PIN_2, INPUT);
+    pinMode(OUT_PIN_1, INPUT);
     if (OCR3A != toggleA[1]) {
       OCR3A = toggleA[1];
       OCR3B = toggleB[1];
     }
 }
 ISR(TIMER3_COMPB_vect) {
+    pinMode(OUT_PIN_1, OUTPUT);
+}
+#endif
+
+#ifdef IN_PIN_2
+ISR(TIMER4_COMPA_vect) {
+    pinMode(OUT_PIN_2, INPUT);
+    if (OCR4A != toggleA[2]) {
+      OCR4A = toggleA[2];
+      OCR4B = toggleB[2];
+    }
+}
+ISR(TIMER4_COMPB_vect) {
     pinMode(OUT_PIN_2, OUTPUT);
+}
+#endif
+
+#ifdef IN_PIN_3
+ISR(TIMER5_COMPA_vect) {
+    pinMode(OUT_PIN_3, INPUT);
+    if (OCR5A != toggleA[3]) {
+      OCR5A = toggleA[3];
+      OCR5B = toggleB[3];
+    }
+}
+ISR(TIMER5_COMPB_vect) {
+    pinMode(OUT_PIN_3, OUTPUT);
 }
 #endif
